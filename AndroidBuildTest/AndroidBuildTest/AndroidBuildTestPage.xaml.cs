dda5687fffe7619baafbe4b3f6eda5f7168d8fd6
Alexander Chocron <alchocro@microsoft.com>
2017-06-21 12:18:34 -0700

    Add project

---
+++ AndroidBuildTest/AndroidBuildTest/AndroidBuildTestPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace AndroidBuildTest
+{
+    public partial class AndroidBuildTestPage : ContentPage
+    {
+        public AndroidBuildTestPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
